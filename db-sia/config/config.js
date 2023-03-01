1142f30683518071d622b92c280262c43a4c2b63
smartinfotama <mysalsabil@gmail.com>
2023-03-01 20:06:58 +0700

    add .env sia

--- db-sia/config/config.js
+++ db-sia/config/config.js
@@ -1,26 +1,26 @@
 "use strict";
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    username: process.env.DB_USER_SIA,
+    password: process.env.DB_PASS_SIA,
     database: process.env.DB_NAME_SIA,
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST_SIA,
     dialect: "mysql",
 		logging: true,
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    username: process.env.DB_USER_SIA,
+    password: process.env.DB_PASS_SIA,
     database: process.env.DB_NAME_SIA,
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST_SIA,
     dialect: "mysql",
     logging: false,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    username: process.env.DB_USER_SIA,
+    password: process.env.DB_PASS_SIA,
     database: process.env.DB_NAME_SIA,
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST_SIA,
     dialect: "mysql",
     // dialectOptions: {
     //   ssl: true,
